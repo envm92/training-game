7471736875e5530b923c4aa91b4e85a4e166ac17
Eri <envm92@gmail.com>
2020-12-03 10:57:26 -0600

    Fix spaces, double equals, obserbable box-count

--- main.js
+++ main.js
@@ -1,188 +1,198 @@
 class BoxGame extends HTMLElement {
-	constructor() {
-		super();
-		const shadow =  this.attachShadow({mode: 'open'});
-		const div = document.createElement('div');
-		div.setAttribute('id', 'box');
-		shadow.appendChild(div);
-		shadow.appendChild(this.getStyles());
-	}
+  constructor() {
+    super();
+    const shadow =  this.attachShadow({mode: 'open'});
+    const div = document.createElement('div');
+    div.setAttribute('id', 'box');
+    shadow.appendChild(div);
+    shadow.appendChild(this.getStyles());
+  }
 
-	static get observedAttributes() { return ['open']; }
+  static get observedAttributes() { return ['open']; }
 
-	getStyles() {
-		const style = document.createElement('style');
-        style.textContent = `
-            div {
-                background: gray;
-                align-items: center;
-                text-shadow: #000 0 0;
-                border-radius: 15px;
-				margin: 10px;
-				height: 60px;
-				width: 60px;
-			}
-			
-		`;
-		return style;
-	}
+  getStyles() {
+    const style = document.createElement('style');
+    style.textContent = `
+      div {
+        background: gray;
+        align-items: center;
+        text-shadow: #000 0 0;
+        border-radius: 15px;
+        margin: 10px;
+        height: 60px;
+        width: 60px;
+      }
+      
+    `;
+    return style;
+  }
 
-	attributeChangedCallback(name, oldValue, newValue) {
-		if (name == 'open') {
-			this.openBox();
-		}
-	}
+  attributeChangedCallback(name, oldValue, newValue) {
+    if (name === 'open') {
+      this.openBox();
+    }
+  }
 
-	openBox() {
-		const shadow = this.shadowRoot;
-		const div = shadow.getElementById('box');
-		div.style.background = 
-			(this.getAttribute('winner') == 'true') ? 'blue': 'red';
-	}
+  openBox() {
+    const shadow = this.shadowRoot;
+    const div = shadow.getElementById('box');
+    div.style.background = 
+      (this.getAttribute('winner') === 'true') ? 'blue': 'red';
+  }
 }
 
 class ScoreGame extends HTMLElement {
-	constructor() {
-		super();
-		const shadow =  this.attachShadow({mode: 'open'});
-		const div = document.createElement('div');
-		const wonDiv = document.createElement('div');
-		const wonH1 = document.createElement('h1');
-		wonH1.textContent = 'Won: ';
-		const wonSpan = document.createElement('span');
-		wonSpan.textContent = this.getAttribute('won') | 0;
-		wonSpan.setAttribute('id', 'won-span');
-		wonH1.appendChild(wonSpan);
-		wonDiv.appendChild(wonH1);
-		div.appendChild(wonDiv);
+  constructor() {
+    super();
+    const shadow =  this.attachShadow({mode: 'open'});
+    const div = document.createElement('div');
+    const wonDiv = document.createElement('div');
+    const wonH1 = document.createElement('h1');
+    wonH1.textContent = 'Won: ';
+    const wonSpan = document.createElement('span');
+    wonSpan.textContent = String(this.getAttribute('won') | 0);
+    wonSpan.setAttribute('id', 'won-span');
+    wonH1.appendChild(wonSpan);
+    wonDiv.appendChild(wonH1);
+    div.appendChild(wonDiv);
 
-		const lostDiv = document.createElement('div');
-		const lostH1 = document.createElement('h1');
-		lostH1.textContent = 'Lost: ';
-		const lostSpan = document.createElement('span');
-		lostSpan.textContent = this.getAttribute('lost') | 0;
-		lostSpan.setAttribute('id', 'lost-span');
-		lostH1.appendChild(lostSpan);
-		lostDiv.appendChild(lostH1);
-		div.appendChild(lostDiv);
+    const lostDiv = document.createElement('div');
+    const lostH1 = document.createElement('h1');
+    lostH1.textContent = 'Lost: ';
+    const lostSpan = document.createElement('span');
+    lostSpan.textContent = String(this.getAttribute('lost') | 0);
+    lostSpan.setAttribute('id', 'lost-span');
+    lostH1.appendChild(lostSpan);
+    lostDiv.appendChild(lostH1);
+    div.appendChild(lostDiv);
 
-		shadow.appendChild(div);
-		shadow.appendChild(this.getStyles());
-	}
+    shadow.appendChild(div);
+    shadow.appendChild(this.getStyles());
+  }
 
-	static get observedAttributes() { return ['won', 'lost']; }
+  static get observedAttributes() { return ['won', 'lost']; }
 
-	attributeChangedCallback(name, oldValue, newValue) {
-		if (name == 'won' || name == 'lost') {
-			const shadow =  this.shadowRoot;
-			const span = shadow.getElementById(`${name}-span`);
-			span.textContent = this.getAttribute(name) | 0;
-		}
-	}
+  attributeChangedCallback(name, oldValue, newValue) {
+    if (name === 'won' || name === 'lost') {
+      const shadow =  this.shadowRoot;
+      const span = shadow.getElementById(`${name}-span`);
+      span.textContent = String(this.getAttribute(name) | 0);
+    }
+  }
 
-	getStyles() {
-		const style = document.createElement('style');
-        style.textContent = `
-            div {
-                background: pink;
-                color: #fff;
-                text-shadow: #000 0 0;
-                border-radius: 15px;
-				width: 300px;
-    			height: 60px;
-				display: flex;
-			}
-			
-		`;
-		return style;
-	}
-	
+  getStyles() {
+    const style = document.createElement('style');
+    style.textContent = `
+      div {
+        background: pink;
+        color: #fff;
+        text-shadow: #000 0 0;
+        border-radius: 15px;
+        width: 300px;
+        height: 60px;
+        display: flex;
+      }
+      
+    `;
+    return style;
+  }
+  
 }
 
 class BoardGame extends HTMLElement {
 
-	boxCount = 5;
-	winner = 0;
-	plays = 0;
-	boxes = [];
+  boxCount = 5;
+  winner = 0;
+  plays = 0;
+  boxes = [];
 
-	constructor() {
-		super();
-		this.boxCount =  this.hasAttribute('box-count') ? this.getAttribute('box-count') : 5;
-		this.won = 0;
-		this.lost = 0;
-		this.drawScore();
-		this.startGame();
-	}
+  constructor() {
+    super();
+    this.boxCount =  this.hasAttribute('box-count') ? this.getAttribute('box-count') : 5;
+    this.won = 0;
+    this.lost = 0;
+    this.drawScore();
+    this.startGame();
+  }
 
-	startGame() {
-		this.initGame();
-		this.drawBoard();
-	}
+  static get observedAttributes() { return ['box-count']; }
 
-	initGame() {
-		this.winner = Math.floor(Math.random() * Math.floor(this.boxCount));
-		this.plays = 0;
-		this.boxes = Array.from({length:this.boxCount},(_, i) => {
-			const box = new BoxGame( this.winner == i );
-			box.setAttribute('winner', this.winner == i);
-			box.addEventListener('click', (_) => {
-				box.setAttribute('open' , '');
-				const isWinner = this.winner == i;
-				this.plays++;
-				if(isWinner) {
-					this.showAlert('You win!!');
-					this.updateScore(isWinner);
-				} else if(!isWinner && this.plays == 3) {
-					this.showAlert('Game over');
-					this.updateScore(isWinner);
-				}
-				return isWinner;
-			});
-			return box;
-		});
-	}
+  attributeChangedCallback(name, oldValue, newValue) {
+    if (name === 'box-count') {
+      this.boxCount = newValue;
+      this.clearBoard();
+      this.startGame();
+    }
+  }
 
-	updateScore(win) {
-		const shadow = this.shadowRoot;
-		const score = shadow.getElementById('score');
-		if (win) {
-			this.won++;
-			score.setAttribute('won', this.won);
-		} else {
-			this.lost++;
-			score.setAttribute('lost', this.lost);
-		}
-	}
+  startGame() {
+    this.initGame();
+    this.drawBoard();
+  }
 
-	showAlert(msg) {
-		setTimeout(() => {
-			alert(msg)
-			this.clearBoard();
-			this.startGame();
-		}, 200);
-	}
+  initGame() {
+    this.winner = Math.floor(Math.random() * Math.floor(this.boxCount));
+    this.plays = 0;
+    this.boxes = Array.from({length:this.boxCount},(_, i) => {
+      const box = new BoxGame( this.winner === i );
+      box.setAttribute('winner', String(this.winner === i));
+      box.addEventListener('click', (_) => {
+        box.setAttribute('open' , '');
+        const isWinner = this.winner === i;
+        this.plays++;
+        if(isWinner) {
+          this.showAlert('You win!!');
+          this.updateScore(isWinner);
+        } else if(!isWinner && this.plays === 3) {
+          this.showAlert('Game over');
+          this.updateScore(isWinner);
+        }
+        return isWinner;
+      });
+      return box;
+    });
+  }
 
-	clearBoard() {
-		const shadow = this.shadowRoot;
-		shadow.removeChild(shadow.getElementById('board'));
-	}
+  updateScore(win) {
+    const shadow = this.shadowRoot;
+    const score = shadow.getElementById('score');
+    if (win) {
+      this.won++;
+      score.setAttribute('won', this.won);
+    } else {
+      this.lost++;
+      score.setAttribute('lost', this.lost);
+    }
+  }
 
-	drawScore() {
-		const shadow = (this.shadowRoot == null) ? this.attachShadow({mode: 'open'}) : this.shadowRoot;
-		const score = new ScoreGame();
-		score.setAttribute('id', 'score');
-		shadow.appendChild(score);
-	}
+  showAlert(msg) {
+    setTimeout(() => {
+      alert(msg)
+      this.clearBoard();
+      this.startGame();
+    }, 200);
+  }
 
-	drawBoard() {
-		const shadow = (this.shadowRoot == null) ? this.attachShadow({mode: 'open'}) : this.shadowRoot;
-		const div = document.createElement('div');
-		div.setAttribute('id', 'board');
-		this.boxes.forEach(box => {div.appendChild(box);});
-		div.style.display = 'flex';
-		shadow.appendChild(div);
-	}
+  clearBoard() {
+    const shadow = this.shadowRoot;
+    shadow.removeChild(shadow.getElementById('board'));
+  }
+
+  drawScore() {
+    const shadow = (this.shadowRoot == null) ? this.attachShadow({mode: 'open'}) : this.shadowRoot;
+    const score = new ScoreGame();
+    score.setAttribute('id', 'score');
+    shadow.appendChild(score);
+  }
+
+  drawBoard() {
+    const shadow = (this.shadowRoot == null) ? this.attachShadow({mode: 'open'}) : this.shadowRoot;
+    const div = document.createElement('div');
+    div.setAttribute('id', 'board');
+    this.boxes.forEach(box => {div.appendChild(box);});
+    div.style.display = 'flex';
+    shadow.appendChild(div);
+  }
 }
 
 customElements.define('score-game', ScoreGame);
